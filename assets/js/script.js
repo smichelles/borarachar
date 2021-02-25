9ae6f135ee4fc202225d101bd0568aa822aab859
Michelle Santos <micksantos10@gmail.com>
2021-02-24 22:09:52 -0300

    v2

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,17 @@ document.querySelector('.modal-overlay')
     .classList.remove('active')
   }
 }
+
+
+// Segundo modal
+
+const Modals = {
+  open(){
+document.querySelector('.modal-overlays')
+               .classList.add('active')
+  },
+  close(){
+    document.querySelector('.modal-overlays')
+    .classList.remove('active')
+  }
+}
